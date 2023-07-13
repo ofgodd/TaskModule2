b6352f9292c475d489e140feac5601fa81e276b9
Arkady Lubarets <djony23rus@gmail.com>
2023-07-14 00:45:09 +0200

    Решил 15!

---
+++ Task15/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //     Даны два числа.Найти их сумму, разность, произведение, 
+            //     а также частное от деления первого числа на второе.
+
+            Console.Write("Введите первое число: ");
+            double enterFirstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            double enterSecondNumber = Convert.ToDouble(Console.ReadLine());
+
+            double resultNumbersSum = enterFirstNumber + enterSecondNumber;
+            Console.WriteLine("Ваша сумма чисел : " + resultNumbersSum);
+
+            double resultNumbersDifferense = enterFirstNumber - enterSecondNumber;
+            Console.WriteLine("Ваша сумма чисел : " + resultNumbersDifferense);
+
+            double resultNumbersFactum = enterFirstNumber * enterSecondNumber;
+            Console.WriteLine("Ваша сумма чисел : " + resultNumbersFactum);
+
+            double resultNumbersNotch = enterFirstNumber / enterSecondNumber;
+            Console.WriteLine("Ваша сумма чисел : " + resultNumbersNotch);
+        }
+    }
+}
