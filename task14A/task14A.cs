d92665e685589a30935f3aef3ca5c46513021b09
Arkady Lubarets <djony23rus@gmail.com>
2023-07-14 00:12:57 +0200

    задача решена!

---
+++ task14A/task14A.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task14A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Даны стороны прямоугольника.Найти его периметр и длину диагонали.
+
+
+        }
+    }
+}
