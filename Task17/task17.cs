26b0e032ed58157b5c45f2464681354ba3e37d65
Arkady Lubarets <djony23rus@gmail.com>
2023-07-17 00:19:02 +0200

    Сделал задачу!24

---
+++ Task17/task17.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task17
+{
+    internal class task17
+    {
+        static void Main(string[] args)
+        {
+            //Известны координаты на плоскости двух точек. 
+            //Составить программу вычисления расстояния между ними.
+
+            Console.Write("Введите первую координату х: ");
+            int firstDote = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите вторая координату х: ");
+            int secondDote = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите первую координату y: ");
+            int thirdDote = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите первую координату y: ");
+            int forthDote = Convert.ToInt32(Console.ReadLine());
+
+            double interval = Math.Sqrt(Math.Pow((secondDote - firstDote), 2)
+                + Math.Pow((forthDote - thirdDote), 2));
+            Console.WriteLine("Расстояние между точками равно :" + interval);
+
+            
+        }
+    }
+}
---
+++ Task24/task24.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task24
+{
+    internal class task24
+    {
+        static void Main(string[] args)
+        {
+            //    Возраст Тани — X лет, а возраст Мити — Y лет. Найти их средний возраст,
+            //    а также определить, на сколько отличается возраст каждого ребенка от среднего значения. 
+
+            Random rnd = new Random();
+            int oldTania = rnd.Next(1, 17);
+            Console.WriteLine("Возраст Тани: " + oldTania);
+
+            int oldMitia = rnd.Next(1, 17);
+            Console.WriteLine("Возраст Тани: " + oldMitia);
+
+            double midleOldChildren = (oldTania + oldMitia) / 2;
+            Console.WriteLine("Средний возраст: " + midleOldChildren);
+
+            double distinctionOldMidleTania = midleOldChildren - oldTania;
+            Console.WriteLine($"На  {distinctionOldMidleTania} отличается " +
+                $"возраст от среднего значения Тани");
+
+            double distinctionOldMidleMitia = midleOldChildren - oldMitia;
+            Console.WriteLine($"На  {distinctionOldMidleMitia} отличается " +
+                $"возраст от среднего значения Мити");
+
+
+        }
+    }
+}
